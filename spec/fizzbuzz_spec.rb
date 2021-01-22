d158fb84b6594bc0b8a44190599c4f8dacf4ec6c
Adam <adam2000phillips@gmail.com>
2021-01-22 14:41:27 +0000

    Second Test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,11 @@ describe 'fizzbuzz' do
   it 'returns fizz when passed(3)' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it 'returns fizz when passed any multiple of 3 and 5' do
+    (1..100).to_a.each do |num|
+      if num.to_f % 15 == 0
+        expect(fizzbuzz(num)).to eq "fizzbuzz"
+      end
+    end
+  end
 end
