86cedeffcc936bb8bfddf9d2da710bcfd5386b99
Adam <adam2000phillips@gmail.com>
2021-01-22 15:11:12 +0000

    Clean up of Tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,25 +10,25 @@ describe 'fizzbuzz' do
       end
     end
   end
-it 'returns buzz when passed any multiple of 5' do
+  it 'returns buzz when passed any multiple of 5' do
     (1..100).to_a.each do |num|
       if num.to_f % 5 == 0 && num.to_f % 15 != 0
         expect(fizzbuzz(num)).to eq "buzz"
       end
     end
-end
-it "return number when not a multple of 3 or 5" do
-  (1..100).to_a.each do |num|
-    if num.to_f % 5 != 0 && num.to_f % 3 != 0
-      expect(fizzbuzz(num)).to eq num
+  end
+  it "return number when not a multple of 3 or 5" do
+    (1..100).to_a.each do |num|
+      if num.to_f % 5 != 0 && num.to_f % 3 != 0
+        expect(fizzbuzz(num)).to eq num
+      end
     end
   end
-end  
-it 'returns fizz when passed any multiple of 3' do
+  it 'returns fizz when passed any multiple of 3' do
     (1..100).to_a.each do |num|
       if num.to_f % 3 == 0 && num.to_f % 15 != 0
         expect(fizzbuzz(num)).to eq "fizz"
       end
     end
-end
+  end
 end
